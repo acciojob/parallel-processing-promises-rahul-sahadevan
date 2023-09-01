5e1d0d22ecf838f157cff45378376fde98e22cde
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-09-01 18:43:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener("click",() =>{
+
+	function download(images){
+		let brr = [];
+		for(let i =0;i<images.length;i++){
+			let p = new Promise((resolve,reject) =>{
+				resolve(images[i].url);
+			})
+			brr.push(p);
+		}
+		return brr;
+		
+	}
+	let x = download(images);
+	let all_val = Promise.all(x);
+	let crr = [];
+	all_val.then((data) =>{
+		for(let i =0;i<data.length;i++){
+			let image = document.createElement("img");
+			image.src = data[i];
+			output.append(image);
+		}
+	})
+	all_val.catch((e) =>{
+		console.log(`Failed to load image's URL: ${e}`);
+	})
+})
+
